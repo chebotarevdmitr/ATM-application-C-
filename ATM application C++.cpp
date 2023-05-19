049524b67cf3df44655b6e9ab7bfe85fdea8f831
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-19 22:41:07 +0300

    calling the menu

--- ATM application C++.cpp
+++ ATM application C++.cpp
@@ -1,10 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
 
+void show_menu() {
+    cout << " ********** MENU ********** " << endl;
+    cout << " 1. Check balance " << endl;
+    cout << " 2. Deposit " << endl;
+    cout << " 3. withdraw " << endl;
+    cout << " *************************** " << endl;
+}
+
+
 int main()
 {
-    cout << "Hello application in C++!\n";
-
+   // check balance, (proverka balansa) deposit,(vnesti dengi na schet) withdraw(snimat dengi)
+   //show menu,(pokazat menu)
+    show_menu();
 
 
     system("pause>0");
