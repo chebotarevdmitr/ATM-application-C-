3ddb58753101983cfe05662cf6edec1ec810c6b9
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-20 00:17:33 +0300

    do while

--- ATM application C++.cpp
+++ ATM application C++.cpp
@@ -15,10 +15,33 @@ int main()
    // check balance, (proverka balansa) deposit,(vnesti dengi na schet) withdraw(snimat dengi)
    //show menu,(pokazat menu)
     int option;
-    show_menu();
-    cout << "Option : ";
-    cin >> option;
+    double  balance = 500;
 
+    do {
+        show_menu();
+        cout << "Option : ";
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            cout << " Balance is: " << balance << "$" << endl; break;
+        case 2:
+            cout << " Deposit amount: ";
+            double deposit_amount;
+            cin >> deposit_amount;
+            balance += deposit_amount;
+            break;
+        case 3:
+            cout << " withdraw  amount: ";
+            double  withdraw_amount;
+            cin >> withdraw_amount;
+            if (withdraw_amount <= balance) balance -= withdraw_amount;
+            else
+                cout << " Not enough money " << endl;
+            break;
+        }
+    } while (option != 4);
     system("pause>0");
 }
 
